75254c55e2361b74cc6a782e1788ba42585a69e7
SergioCasallas <sergiocasallas01@gmail.com>
2022-03-18 09:25:47 -0500

    Change Response Json To Text

--- src/controllers/QRController.js
+++ src/controllers/QRController.js
@@ -1,16 +1,20 @@
 const QRCode = require("qrcode");
 
 exports.createQR = async (req, res) => {
-  const { url } = req.body;
+  const { url, numeroTRA, codigoVerificacion } = req.body;
+
+  const urlParams = `${url}/${numeroTRA}/${codigoVerificacion}`;
 
   const generateQR = (text) => {
     try {
+      // return QRCode.toDataURL(text);
       return QRCode.toDataURL(text);
     } catch (e) {
       console.log(e);
     }
   };
 
-  const QR = await generateQR(url);
-  res.json({ mensaje: `${await QR}` });
+  const QR = await generateQR(urlParams);
+  const QRSlice = await QR.slice(22);
+  res.send(`${await QRSlice}`);
 };
